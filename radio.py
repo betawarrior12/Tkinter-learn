a20dcdf4fd163d28538063bb93ebb33759121c55
betawarrior12 <adelabuoluwapemi@gmail.com>
2022-12-05 22:16:44 +0100

    Added icons

---
+++ radio.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+
+
+root = Tk()
+root.title("Code with Betawarrior12")
+root.iconbitmap("C:\\Users\\USER\\Pemi\\PYTHON\\Tkinter-learn\\Images\\Naruto2.ico")
+
+
+
+
+
+root.mainloop()
+'''
