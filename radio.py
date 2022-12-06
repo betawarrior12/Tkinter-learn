84ce2185a607c057d05d831e0270ab8ef03b43e8
betawarrior12 <adelabuoluwapemi@gmail.com>
2022-12-06 08:45:31 +0100

    New tutorial: radio buttons

--- radio.py
+++ radio.py
@@ -1,15 +1,41 @@
 from tkinter import *
 from PIL import ImageTk, Image
-import os
 
 
 root = Tk()
 root.title("Code with Betawarrior12")
 root.iconbitmap("C:\\Users\\USER\\Pemi\\PYTHON\\Tkinter-learn\\Images\\Naruto2.ico")
 
+#r = IntVar()
+#r.set("2")
 
+TOPPINGS = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion"),
+]
+
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+
+for text, topping in TOPPINGS:
+    Radiobutton(root, text=text, variable=topping, value=mode).pack(anchor=W)
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()
+
+#Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+#Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+
+#myLabel = Label(root, text=pizza.get())
+#myLabel.pack()
+
+myButton = Button(root, text="Click Me", command=lambda: clicked(pizza.get()))
+myButton.pack()
 
 
 
 root.mainloop()
-'''
